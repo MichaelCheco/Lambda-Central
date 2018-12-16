6245bcfe04c349fe8f08f072890f71054fb455ff
Michael Checo <checomichael2@gmail.com>
2018-12-15 21:45:32 -0400

    all dependencies needed for backened

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
 const { GraphQLServer } = require('graphql-yoga');
-
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    track: 'Web Development',
+    title: 'Learn GraphQL!',
+    description: 'GraphQL is a query language for APIs and a runtime for fulfilling those queries with your existing data.'
+}]
 const typeDefs =`
   type Query {
       info: String!
+      feed: [Link!]!
+  }
+  type Link {
+      id: ID!
+      track: String!
+      title: String!
+      description: Post!
+
+      type Post {
+          content: String!
+      }
   }
 `
 
 const resolvers = {
     Query: {
-        info: () => `This is the API of my Lambda Central project`
+        info: () => `This is the API of my Lambda Central project`,
+        feed: () => links
     }
 }
 
