81a231411a02d09da768205a6b819182bfc3be2f
Michael Checo <checomichael2@gmail.com>
2018-12-19 11:16:43 -0400

    utilities and imports

---
+++ src/utils.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken')
+const APP_SECRET = 'sfvrle34tdsv'
+
+function getUserId(context) {
+    const Authorization = context.request.get('Authorization')
+    if(Authorization) {
+        const token = Authorization.replace('Bearer ', '')
+        const { userId } = jwt.verify(token, APP_SECRET)
+        return userId
+    }
+    throw new Error('Not authenticated')
+}
+
+module.exports = {
+    APP_SECRET,
+    getUserId
+}
