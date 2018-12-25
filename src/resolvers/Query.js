03b7cd66b84e41a41e521f817e85569a5616822f
Michael Checo <checomichael2@gmail.com>
2018-12-25 11:02:10 -0400

    can now querylist of tracks

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,7 +1,11 @@
 function feed(parent, args, context, info) {
     return context.prisma.links()
 }
+function tracks(parent, args, context, info) {
+    return context.prisma.tracks()
+}
 
 module.exports = {
     feed,
+    tracks,
 }
